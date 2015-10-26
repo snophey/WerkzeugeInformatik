c507b89d865c43fb54d604cb3b1d577c19b9467f
Alex Rovner <rovnero@gmail.com>
2015-10-26 12:14:01 +0100

    Finished the actual ex 4.

---
+++ Aufgabe 4/Aufgabe1/aufg1.mm
@@ -0,0 +1,53 @@
+<map version="1.0.1">
+
+<node  TEXT="Betriebssysteme">
+<node   POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+
+  </head>
+  <body>
+    <img src="apple.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+
+<node  POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+
+  </head>
+  <body>
+    <img src="linux.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+
+<node   POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+
+  </head>
+  <body>
+    <img src="microsoft.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+
+<node   POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+
+  </head>
+  <body>
+    <img src="android.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+
+</node>
+</map>
